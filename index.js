1ba525e13b5cae0bf0d901f19aea6fb071efdede
Hexhu <i@hexhu.net>
2020-10-22 01:44:04 -0700

    manage with CloudFlare wrangler; remember to update CF secret env var BOT_TOKEN and SECRET_WEBHOOK_PATH on change

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const { Telegraf } = require('telegraf');
+const { Telegraf } = require('telegraf'); // A Telegram Bot Framework
 const { Application, Router } = require('@cfworker/web');
+// Make telegraf work with cf worker
 const createTelegrafMiddware = require('cfworker-middware-telegraf');
 
-const bot = new Telegraf('BOT_TOKEN');
+const bot = new Telegraf(BOT_TOKEN);
 
-// Your code here, but do not `bot.launch()`
+// Main logic here, but don't do `bot.launch()`, as it is handled later
+bot.command('foo', (ctx) => ctx.reply('bar'))
 
+// Handle the rest
 const router = new Router();
-router.post('/SECRET_PATH', createTelegrafMiddware(bot));
+router.post(SECRET_WEBHOOK_PATH, createTelegrafMiddware(bot));
 new Application().use(router.middleware).listen();
